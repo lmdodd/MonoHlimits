4715c231b47368a682cbda75395ab359d38fec93
Laura Dodd <ldodd@cern.ch>
2017-05-08 22:59:51 +0200

    remove file

--- makeImpactsUnblind.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-#example commands to run impact plots, any signal directory is ok. 
-
-combineTool.py -M Impacts -m 120 -d card.root  --doInitialFit --rMin -20 --rMax 20 --robustFit 1 --expectSignal=1  --parallel 8
-combineTool.py -M Impacts -m 120 -d card.root  --robustFit 1 --rMin -20 --rMax 20  --doFits --expectSignal=1  --parallel 8
-combineTool.py -M Impacts -m 120 -d card.root  -o impacts.json
-plotImpacts.py -i impacts.json -o impacts
